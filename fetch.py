adec7c7e6a2052c9ee23630dcfb521bd011563ab
travic <art.lolos1@gmail.com>
2013-12-26 20:05:35 -0500

    Fetches a list in the database

---
+++ fetch.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import psycopg2
+import sys
+
+con = None
+
+try:
+
+    con = psycopg2.connect(database='testdb', user='travic')
+
+
+    cur = con.cursor()
+    cur.execute("SELECT * FROM cars")
+
+    rows = cur.fetchall()
+ 
+    for row in rows:
+        print row
+
+
+except psycopg2.DatabaseError, e:
+    print 'Error %s' %e
+    sys.exit(1)
+
+
+
+finally:
+
+    if con:
+       con.close()
